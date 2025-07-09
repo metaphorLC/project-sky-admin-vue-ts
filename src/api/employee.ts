ce0df63abd8f8fbbfc9465bdfe91d8bea2fd63fe
Lin <hhharrisonlin@gmail.com>
2025-07-09 08:04:16 +0800

    Initial project-sky-admin-vue-ts

---
+++ src/api/employee.ts
@@ -0,0 +1,37 @@
+import request from '@/utils/request'
+/**
+ *
+ * 员工管理
+ *
+ **/
+// 登录
+export const login = (data: any) =>
+  request({
+    'url': '/employee/login',
+    'method': 'post',
+    data: data
+  })
+
+  // 退出
+ export const userLogout = (params: any) =>
+ request({
+   'url': `/employee/logout`,
+   'method': 'post',
+   params
+ })
+
+ // 分页查询
+ export const getEmployeeList = (params: any) =>
+ request({
+   'url': `/employee/page`,
+   'method': 'get',
+   'params': params
+ }) 
+
+ // 启用禁用员工账号
+ export const enableOrDisableEmployee = (params: any) =>
+ request({
+   'url': `/employee/status/${params.status}`,
+   'method': 'post',
+   'params': {id: params.id}
+ }) 
